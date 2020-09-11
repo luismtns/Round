249a15245deb2c0621fce71d4a9169e0f33a4e76
DiegoBuzanello - Desktop <buzanello.diego@gmail.com>
2020-09-11 19:28:22 -0300

    mobile

---
+++ mobile/src/pages/Landing/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+
+function Landing() {
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>
+                Começo do Round uhuuu
+            </Text>
+        </View>
+    );
+}
+
+export default Landing;
---
+++ mobile/src/pages/Landing/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#6959CD',
+        padding: 20,
+        alignItems: 'center',
+        justifyContent:'center'
+    },
+    text: {
+        fontSize: 22,
+        color: '#ffffff',
+        width: 200,
+        height: 100,
+        textAlign: 'center'
+    }
+});
+
+export default styles;

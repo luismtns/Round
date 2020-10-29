002a14b9f332c0cd05d3789917aa0a13df16001c
Luis Bovo <luis.bovo@bit.com.br>
2020-10-28 22:40:41 -0300

    next build config

--- web/next.config.js
+++ web/next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  /* config options here */
+  images: {
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    domains: [
+      "round-landing-page.luismtns.vercel.app",
+      "round-landing-page.vercel.app",
+      "round-landing-page-git-master.luismtns.vercel.app",
+    ],
+    path: "/_next/image",
+    loader: "default",
+  },
 };

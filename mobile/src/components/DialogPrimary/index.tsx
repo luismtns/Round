902258807bc7b6733f3908d5005be1fcb01c2c8b
DiegoBuzanello - Desktop <buzanello.diego@gmail.com>
2020-10-30 23:56:21 -0300

    dialog component

---
+++ mobile/src/components/DialogPrimary/index.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { View } from "react-native";
+import { Button, Paragraph, Dialog, Portal } from "react-native-paper";
+
+const DialogPrimary = ({
+  title,
+  paragraph,
+  button,
+  buttonAction,
+  show,
+  hide,
+  action,
+}: any) => {
+  const visible = show ? true : false;
+
+  return (
+    <View>
+      <Portal>
+        <Dialog visible={visible}>
+          <Dialog.Title>{title}</Dialog.Title>
+          <Dialog.Content>
+            <Paragraph>{paragraph}</Paragraph>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={hide}>{button}</Button>
+            <Button onPress={action}>{buttonAction}</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+    </View>
+  );
+};
+
+export default DialogPrimary;

61cf3c8cf1cd2c965a4574e034f428857b8c53b1
Diego Buzanello <buzanello.diego@gmail.com>
2020-11-10 21:40:55 -0300

    env fix

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,10 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      'babel-preset-expo'
-    ],
-    plugins: ['inline-dotenv'],
-    // plugins: ["transform-inline-environment-variables"] 
+    presets: ["babel-preset-expo"],
+    plugins: ["inline-dotenv", "transform-inline-environment-variables"],
   };
 };
